6c964705423730c487e9ed2047ce449c7db92425
JCortes1-181 <285882489+JCortesDiaz2512@users.noreply.github.com>
2026-06-23 09:44:18 -0400

    1.- Base funcionando

---
+++ pruebafinalJcortes/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log("Servidor de la Pokédex corriendo en http://localhost:3000");
+});
